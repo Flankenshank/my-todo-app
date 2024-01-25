2b3c2b0cd910c533b578b0b21f68a16cc73d9e45
dylan <dylan.date@gmail.com>
2024-01-25 20:10:40 +0200

    Initial commit

--- web.py
+++ web.py
@@ -18,5 +18,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="new_todo", placeholder="Add a new todo...",
+st.text_input(label="Add a new todo", placeholder="Add a new todo...",
               on_change=add_todo, key='new_todo')
